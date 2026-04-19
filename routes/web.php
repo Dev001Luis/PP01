94f22b77fae070f2f2c4bf8f88f62d4da79f9b0a
Luis Stasi <stasi93@libero.it>
2026-04-19 15:03:12 +0100

    structure: define web routes and PortfolioController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PortfolioController::class, 'index'])->name('home');
+Route::post('/contact', [PortfolioController::class, 'contact'])->name('contact.submit');
