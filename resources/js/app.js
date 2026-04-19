f73da275d09abdab4df28c099c59cf83c2b35fc2
Luis Stasi <stasi93@libero.it>
2026-04-19 15:01:14 +0100

    feat: implement color palette and asset pipeline

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 //
+import './bootstrap';
+import $ from 'jquery';
+import 'particles.js';
+
+window.$ = window.jQuery = $;
+
+// Initialize Particles
+particlesJS.load('particles-js', '/particlesjs-config.json', function() {
+    console.log('PP01: Neural Network Online');
+});
